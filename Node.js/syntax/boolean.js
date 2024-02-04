31751590fecfa5a48bab390131f3307fa0987ee7
lyacinth <102397313+lyacinth@users.noreply.github.com>
2024-02-04 15:42:43 +0900

    2024.02.04

---
+++ Node.js/syntax/boolean.js
@@ -0,0 +1,3 @@
+console.log(true);
+console.log(false);
+//boolean에는 true와 false가 있음
---
+++ Node.js/syntax/comparison.js
@@ -0,0 +1,5 @@
+console.log(1==1); //ture
+console.log(1==2); //false
+console.log(1>2); //false
+console.log(1<2); //true
+// = 대입연산자 == 비교연산자
